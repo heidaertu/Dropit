191b652d2a84abe1dcf1255c35809e712c671ca6
heidaertu <993409712@qq.com>
2015-10-23 19:47:04 +0800

    dropit

---
+++ ios_upload/dropit/dropitAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  dropitAppDelegate.h
+//  dropit
+//
+//  Created by wangdongfang on 14-9-18.
+//  Copyright (c) 2014年 _. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dropitAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
