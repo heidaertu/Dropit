35639e53550967e09eeb25958a4dee223d06b8dd
heidaertu <993409712@qq.com>
2015-10-23 19:15:10 +0800

    dropit_game

---
+++ github/ios_game/dropitTests/dropitTests.m
@@ -0,0 +1,34 @@
+//
+//  dropitTests.m
+//  dropitTests
+//
+//  Created by wangdongfang on 14-9-18.
+//  Copyright (c) 2014年 _. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface dropitTests : XCTestCase
+
+@end
+
+@implementation dropitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
